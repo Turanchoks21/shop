5c06eb57bbdb9115663838c602cfcfa26bd3abca
Turanchoks21 <manjak2111@gmail.com>
2024-07-27 12:44:16 +0300

    feat(navigations): created NavBar

---
+++ src/components/navigations/NavBar.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import NavButton from "../buttons/NavButton";
+import { Bars3Icon } from "@heroicons/react/20/solid";
+import Logo from "../Logo";
+import { Link } from "react-router-dom";
+import clsx from "clsx";
+
+function NavBar() {
+  const { t, i18n } = useTranslation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
+  const languages = ["ru", "en"];
+  const [currentLangIndex, setCurrentLangIndex] = useState(
+    languages.indexOf(i18n.language)
+  );
+
+  function changeLanguage() {
+    const nextLangIndex = (currentLangIndex + 1) % languages.length;
+    i18n.changeLanguage(languages[nextLangIndex]);
+    setCurrentLangIndex(nextLangIndex);
+  }
+
+  return (
+    <>
+      <div
+        className="fixed items-center top-0 left-0 z-10 w-full mx-auto px-2 xxl:py-5 sm:px-6 lg:px-56 
+      bg-analytics-azule
+      border-blue-purple transition-color ease-in-out duration-500
+      text-white
+      "
+      >
+        <div className="relative flex items-center justify-between h-16">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <Bars3Icon
+              className="h-8 text-chiper-chartreuse"
+              onClick={toggleMenu}
+            />
+          </div>
+          <div className="flex-1 flex justify-center sm:items-stretch sm:justify-start">
+            <div className="flex-shrink-0">
+              <Link>
+                <Logo />
+              </Link>
+            </div>
+            <div className="hidden sm:block sm:ml-6">
+              <div className="flex justify-center items-center space-x-4">
+                <NavButton to="/register">{t("register")}</NavButton>
+                <NavButton to="/login">{t("login")}</NavButton>
+              </div>
+            </div>
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <div className="flex space-x-3">
+              <button
+                className="text-xl font-semibold text-chiper-chartreuse"
+                onClick={changeLanguage}
+              >
+                {i18n.language.toUpperCase()}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* mob menu */}
+      <div
+        className={clsx("fixed z-20 inset-0 bg-gray-800/65 ", {
+          hidden: !isMenuOpen,
+        })}
+        onClick={toggleMenu}
+      />
+      <div
+        className={clsx(
+          "fixed inset-0 bg-analytics-azule z-30 transform transition-all duration-500",
+          { "-translate-x-full": !isMenuOpen, "-translate-x-[50%]": isMenuOpen }
+        )}
+      >
+        <div className="py-4">
+          <div className="flex justify-between w-full">
+            <div className="w-full" />
+            <div className="flex w-full flex-col items-center gap-y-4">
+              <NavButton to="/" onClick={toggleMenu}>
+                {t("homePage")}
+              </NavButton>
+              <NavButton to="/register" onClick={toggleMenu}>
+                {t("register")}
+              </NavButton>
+              <NavButton to="/login" onClick={toggleMenu}>
+                {t("login")}
+              </NavButton>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default NavBar;
