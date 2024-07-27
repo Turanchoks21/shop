6a080e0b8cdd0e3f8277d940f90908b1e14ed165
Turanchoks21 <manjak2111@gmail.com>
2024-07-27 12:43:32 +0300

    feat(components): refactor

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,17 +1,14 @@
-import { Link } from "react-router-dom";
-
 function Logo() {
   return (
     <>
-      <Link
-        to="/"
+      <div
         className="bg-clip-text text-transparent bg-gradient-to-r from-gigabyte-green
        via-chiper-chartreuse to-gigabyte-green"
       >
         <div className="font-bold text-2xl xxl:text-4xl 3xl:text-6xl 5xl:text-7xl">
           Game Start
         </div>
-      </Link>
+      </div>
     </>
   );
 }
