04025a95509ca4ffa57ce4ffab1e9ad2a88e8377
Turanchoks21 <manjak2111@gmail.com>
2024-07-27 11:20:19 +0300

    feat: added react router

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import "./i18n";
 import App from "./App.jsx";
+import HomeView from "./views/HomeView.jsx";
+import LoginView from "./views/LoginView.jsx";
+import RegisterView from "./views/RegisterView.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomeView />,
+      },
+      {
+        path: "login",
+        element: <LoginView />,
+      },
+      {
+        path: "register",
+        element: <RegisterView />,
+      },
+    ],
   },
 ]);
 
@@ -16,4 +34,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-
