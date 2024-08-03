abfc35bb26f0f18227fa91b8a161f293fbce295d
Turanchoks21 <manjak2111@gmail.com>
2024-08-04 01:10:00 +0300

    feat(views): added page layout

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,12 +1,15 @@
 function HomeView() {
-    return (
-      <>
-        <div className="items-center mt-16">
-      
+  return (
+    <>
+      <div className="items-center mt-16 py-8">
+        <div className="/* ыв */ h-72"></div>
+        <div className="border-2 mt-8 h-12"></div>
+        <div className="mt-8 h-screen bg-white rounded-md p-8">
+          <div className="border-2 border-black p-4"></div>
         </div>
-      </>
-    );
-  }
-
+      </div>
+    </>
+  );
+}
 
 export default HomeView;
