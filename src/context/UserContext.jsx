a0be4776e54903c689106ea10ffd6d26cbf73a79
Turanchoks21 <manjak2111@gmail.com>
2024-08-07 15:05:38 +0300

    feat(context): created user context

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,33 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+
+const UsersContext = createContext();
+
+export const useUsers = () => useContext(UsersContext);
+
+export function UsersProvider({ children }) {
+  const [users, setUsers] = useState(() => {
+    const storedUsers = localStorage.getItem("users");
+    return storedUsers ? JSON.parse(storedUsers) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("users", JSON.stringify(users));
+  }, [users]);
+
+  function addUser(user) {
+    setUsers((prevUsers) => [...prevUsers, user]);
+  }
+
+  function clearUsers() {
+    setUsers([]);
+    localStorage.removeItem("users");
+  }
+
+  return (
+    <UsersContext.Provider value={{ users, addUser, clearUsers }}>
+      {children}
+    </UsersContext.Provider>
+  );
+}
+
+export default UsersProvider;
