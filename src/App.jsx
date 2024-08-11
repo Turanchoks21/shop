9154ef2c9ee60b05d5877b6c99bcebf8d32253c3
Turanchoks21 <manjak2111@gmail.com>
2024-08-11 09:55:51 +0300

    feat: added ScrollToTop

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/navigations/NavBar";
+import ScrollToTop from "./router/ScrollToTop";
+import { UsersProvider } from "./context/UserContext";
 
 function App() {
   return (
     <>
-      <NavBar />
-      <div className="m-0 px-5 lg:px-48">
-        <Outlet />
-      </div>
+      <UsersProvider>
+        <NavBar />
+        <div className="m-0 px-2 lg:px-48">
+          <ScrollToTop>
+            <Outlet />
+          </ScrollToTop>
+        </div>
+      </UsersProvider>
     </>
   );
 }
