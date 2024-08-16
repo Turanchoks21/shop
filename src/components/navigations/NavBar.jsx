882c97bfb93ddd15dcc8e78c4cb1d69042e54986
Turanchoks21 <manjak2111@gmail.com>
2024-08-16 22:15:41 +0300

    refactor

--- src/components/navigations/NavBar.jsx
+++ src/components/navigations/NavBar.jsx
@@ -1,19 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useContext } from "react";
+import axios from "axios";
 import { useTranslation } from "react-i18next";
 import NavButton from "../buttons/NavButton";
 import {
   Bars3Icon,
-  ArrowRightEndOnRectangleIcon,
-  UserIcon,
+  ArrowRightOnRectangleIcon,
+  ShoppingCartIcon,
 } from "@heroicons/react/20/solid";
 import Logo from "../Logo";
 import { Link } from "react-router-dom";
 import clsx from "clsx";
+import FlyoutMenu from "../ui/FlyoutMenu";
+import CartModal from "../modals/CartModal";
+import { CartContext } from "../../context/CartContext";
+import { useUsers } from "../../context/UserContext";
+import AccountReplenishmentModal from "../modals/AccountReplenishmentModal";
 
 function NavBar() {
   const { t, i18n } = useTranslation();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
   const [isUserLogined, setIsUserLogined] = useState(false);
+  const [isReplenishmentModal, setIsReplenishmentModal] = useState(false);
+  const [balance, setBalance] = useState(null);
+  const userMenuRef = useRef(null);
+  const [isCartModalOpen, setIsCartModalOpen] = useState(false);
+  const { cart, getTotalItemCount } = useContext(CartContext);
+  const { users } = useUsers();
+  const username = users.length > 0 ? users[0] : "Guest";
 
   useEffect(() => {
     const storedUsers = localStorage.getItem("users");
@@ -23,10 +37,60 @@ function NavBar() {
         setIsUserLogined(true);
       }
     }
-  }, []);
+
+    // Получение баланса пользователя
+    if (username && username !== "Guest") {
+      const fetchBalance = async () => {
+        try {
+          const response = await axios.get(
+            `http://8ybg5l.realhost-free.net/Payment/GetWalletCount?userName=${username}`
+          );
+          if (response.status === 200) {
+            setBalance(response.data); // Сохраняем баланс в состоянии
+          }
+        } catch (error) {
+          console.error("Error fetching balance:", error);
+        }
+      };
+
+      fetchBalance();
+    }
+
+    window.addEventListener("storage", handleStorageChange);
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [username]);
+
+  const handleStorageChange = () => {
+    const storedUsers = localStorage.getItem("users");
+    if (storedUsers) {
+      const users = JSON.parse(storedUsers);
+      setIsUserLogined(users.length > 0);
+    } else {
+      setIsUserLogined(false);
+    }
+  };
+
+  const handleClickOutside = (event) => {
+    if (userMenuRef.current && !userMenuRef.current.contains(event.target)) {
+      setIsUserMenuOpen(false);
+    }
+  };
 
   function toggleMenu() {
     setIsMenuOpen(!isMenuOpen);
+  }
+
+  function toggleCartModal() {
+    setIsCartModalOpen(!isCartModalOpen);
+  }
+
+  function toggleReplenishmentModal() {
+    setIsReplenishmentModal(!isReplenishmentModal);
   }
 
   const languages = ["ru", "en"];
@@ -58,32 +122,45 @@ function NavBar() {
           </div>
           <div className="flex-1 flex justify-center sm:items-stretch sm:justify-start">
             <div className="flex-shrink-0">
-              <Link>
+              <Link to="/">
                 <Logo />
               </Link>
             </div>
             <div className="hidden sm:block sm:ml-6">
               <div className="flex justify-center items-center space-x-4">
-                <NavButton>{t("gameKeys")}</NavButton>
-                <NavButton>{t("accounts")}</NavButton>
-                <NavButton>{t("items")}</NavButton>
+                <NavButton
+                  onClick={() => {
+                    toggleReplenishmentModal();
+                  }}
+                >
+                  {t("accountReplenishment")}
+                </NavButton>
+                <NavButton to="favorite">{t("favorite")}</NavButton>
+                <NavButton to="history">{t("history")}</NavButton>
               </div>
             </div>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <div className="flex space-x-3">
+            <div className="flex space-x-3 items-center">
               {isUserLogined ? (
-                <Link to="/profile">
-                  <UserIcon className="h-8 text-chiper-chartreuse" />
-                </Link>
+                <div className="hidden pt-2 2lg:flex justify-between ">
+                  <FlyoutMenu balance={balance} />
+                </div>
               ) : (
                 <Link to="/login">
-                  <ArrowRightEndOnRectangleIcon className="h-8 text-chiper-chartreuse" />
+                  <ArrowRightOnRectangleIcon className="h-8 text-chiper-chartreuse" />
                 </Link>
               )}
-
+              <button onClick={toggleCartModal} className="relative">
+                <ShoppingCartIcon className="h-7 text-chiper-chartreuse" />
+                {getTotalItemCount() > 0 && (
+                  <span className="absolute -top-2 -right-2 bg-red-600 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
+                    {getTotalItemCount()}
+                  </span>
+                )}
+              </button>
               <button
-                className="text-xl font-semibold text-chiper-chartreuse"
+                className="text-xl font-semibold text-chiper-chartreuse hidden sm:block"
                 onClick={changeLanguage}
               >
                 {i18n.language.toUpperCase()}
@@ -105,20 +182,56 @@ function NavBar() {
           { "-translate-x-full": !isMenuOpen, "-translate-x-[50%]": isMenuOpen }
         )}
       >
-        <div className="py-4">
+        <div className="">
           <div className="flex justify-between w-full">
             <div className="w-full" />
             <div className="flex w-full flex-col items-center gap-y-4">
+              <div className="border-b w-full p-2 text-chiper-chartreuse">
+                <div className="flex justify-between ">
+                  <span>{username}</span>
+                  <button
+                    className="text-xl font-semibold text-chiper-chartreuse"
+                    onClick={changeLanguage}
+                  >
+                    {i18n.language.toUpperCase()}
+                  </button>
+                </div>
+                <div className="flex space-x-2">
+                  <span>
+                    {t("balance")}:
+                    {balance !== null ? `${balance}` : "Loading..."}
+                  </span>
+                </div>
+              </div>
               <NavButton to="/" onClick={toggleMenu}>
                 {t("homePage")}
               </NavButton>
-              <NavButton>Игровые ключи</NavButton>
-              <NavButton>Аккаунты</NavButton>
-              <NavButton>Предметы</NavButton>
+              <NavButton
+                onClick={() => {
+                  toggleReplenishmentModal();
+                  toggleMenu();
+                }}
+              >
+                {t("accountReplenishment")}
+              </NavButton>
+              <NavButton to="favorite" onClick={toggleMenu}>
+                {t("favorite")}
+              </NavButton>
+              <NavButton to="history" onClick={toggleMenu}>
+                {t("history")}
+              </NavButton>
             </div>
           </div>
         </div>
       </div>
+      <CartModal
+        isOpen={isCartModalOpen}
+        onClose={() => setIsCartModalOpen(false)}
+      />
+      <AccountReplenishmentModal
+        isOpen={isReplenishmentModal}
+        onClose={() => setIsReplenishmentModal(false)}
+      />
     </>
   );
 }
