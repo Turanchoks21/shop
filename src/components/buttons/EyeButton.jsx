964d5ec5add5f2e8d364fe83fa0e3920d05b0add
Turanchoks21 <manjak2111@gmail.com>
2024-08-16 22:13:03 +0300

    refactor

--- src/components/buttons/EyeButton.jsx
+++ src/components/buttons/EyeButton.jsx
@@ -2,7 +2,7 @@ import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
 
 function EyeButton({ onClick, isPasswordVisible }) {
   return (
-    <div
+    <button
       onClick={onClick}
       className="text-chiper-chartreuse hover:text-chiper-chartreuse/60 cursor-pointer"
     >
@@ -11,7 +11,7 @@ function EyeButton({ onClick, isPasswordVisible }) {
       ) : (
         <EyeSlashIcon className="h-7" />
       )}
-    </div>
+    </button>
   );
 }
 
