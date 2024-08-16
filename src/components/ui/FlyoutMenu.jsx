5016dfa006a92e42c4f6095cb416088a2d591a49
Turanchoks21 <manjak2111@gmail.com>
2024-08-16 22:16:30 +0300

    refactor

--- src/components/ui/FlyoutMenu.jsx
+++ src/components/ui/FlyoutMenu.jsx
@@ -2,12 +2,26 @@ import { Fragment } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import { useTranslation } from "react-i18next";
 import { useState } from "react";
-import { Cog6ToothIcon } from "@heroicons/react/24/outline";
-import { UserIcon } from "@heroicons/react/20/solid";
-import { Link } from "react-router-dom";
+import { BanknotesIcon, UserIcon } from "@heroicons/react/20/solid";
+import { useUsers } from "../../context/UserContext";
+import { useNavigate } from "react-router-dom";
+import AccountReplenishmentModal from "../modals/AccountReplenishmentModal";
 
-export default function Example() {
+export default function FlyotMenu({ balance }) {
   const { t } = useTranslation();
+  const { users, clearUsers } = useUsers();
+  const username = users.length > 0 ? users[0] : "Guest";
+  const [isReplenishmentModal, setIsReplenishmentModal] = useState(false);
+  const navigate = useNavigate(); 
+
+  function toggleReplenishmentModal() {
+    setIsReplenishmentModal(!isReplenishmentModal);
+  }
+
+  function handleLogout() {
+    clearUsers(); 
+    navigate("/login");
+  }
 
   return (
     <>
@@ -33,21 +47,22 @@ export default function Example() {
               className="absolute z-10 2lg:left-1/2 transform -translate-x-1/2 mt-3 px-1
                w-52 sm:px-0"
             >
-              <div className="rounded-lg shadow-lg ring-1 ring-opacity-5 overflow-hidden">
-                <div className="grid grid-cols-2 gap-6 bg-white px-2 py-4 sm:p-6">
-                  <div>
-                    <Link to="/profile">
-                      <Cog6ToothIcon className="h-8 text-black" />
-                    </Link>
+              <div className="rounded-lg shadow-md shadow-chiper-chartreuse ring-0 ring-opacity-5 overflow-hidden">
+                <div className="flex text-chiper-chartreuse flex-col gap-4 bg-analytics-azule px-2 py-4 sm:p-6">
+                  <div className="text-center text-xl font-semibold ">
+                    <div>{username}</div>
+                    <div className="text-xl flex justify-center items-center space-x-1">
+                      <span>{balance}</span>
+                      <BanknotesIcon className="h-6" />
+                    </div>
                   </div>
-                  <div>
-                    <Cog6ToothIcon className="h-8 text-black" />
-                  </div>
-                  <div>
-                    <Cog6ToothIcon className="h-8 text-black" />
-                  </div>
-                  <div>
-                    <Cog6ToothIcon className="h-8 text-black" />
+                  <div className="p-4 w-full">
+                    <button
+                      className="bg-red-500 w-full p-2 rounded-lg font-semibold text-white hover:bg-red-500/80"
+                      onClick={handleLogout}
+                    >
+                      {t("logout")}
+                    </button>
                   </div>
                 </div>
               </div>
@@ -55,6 +70,10 @@ export default function Example() {
           </Transition>
         </>
       </Popover>
+      <AccountReplenishmentModal
+        isOpen={isReplenishmentModal}
+        onClose={() => setIsReplenishmentModal(false)}
+      />
     </>
   );
 }
